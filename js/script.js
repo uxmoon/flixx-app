807fbda5781b2e5a8298a19e6242efcfa05ca95d
uxmoon <uxmoon@gmail.com>
2023-04-14 15:46:14 -0300

    Add router to run code for each page

--- js/script.js
+++ js/script.js
@@ -1,1 +1,30 @@
-console.log('flixx app')
+const global = {
+  currentPage: window.location.pathname,
+}
+// Create a simple router
+console.log(global.currentPage)
+
+function init() {
+  switch (global.currentPage) {
+    case '/':
+    case '/index.html':
+      console.log('home page')
+      break
+    case '/movie-details.html':
+      console.log('details: movie')
+      break
+    case '/search.html':
+      console.log('search')
+      break
+    case '/shows.html':
+      console.log('shows')
+      break
+    case '/tv-details.html':
+      console.log('details: tv')
+      break
+    default:
+      break
+  }
+}
+
+init()
