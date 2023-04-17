3e5f7105dfa44284ee3f11a2f497c46a4e8d0376
uxmoon <uxmoon@gmail.com>
2023-04-17 16:22:42 -0300

    Hide and show spinner, display popular shows

--- js/script.js
+++ js/script.js
@@ -27,14 +27,42 @@ const displayPopularMovies = async () => {
   })
 }
 
+// Fetch and display popular movies
+const displayPopularShows = async () => {
+  const { results } = await fetchAPIData('tv/popular')
+
+  results.forEach((show) => {
+    const div = document.createElement('div')
+    div.classList.add('card')
+    div.innerHTML = `
+      <a href="tv-details.html?id=${show.id}">
+      ${
+        show.poster_path
+          ? `<img src="https://image.tmdb.org/t/p/w500${show.poster_path}" alt="${show.name}">`
+          : `<img src="images/no-image.jpg" alt="${show.name}">`
+      } 
+      </a>
+      <div class="card-body">
+        <h3>${show.name}</h3>
+        <p>Release date: ${show.first_air_date}</p>
+      </div>
+    `
+    document.getElementById('popular-shows').appendChild(div)
+  })
+}
+
 // Fetch data from TMDB API
 const fetchAPIData = async (endpoint) => {
   const API_KEY = 'f2ea09585c04240200255b651d9f228b'
   const API_URL = 'https://api.themoviedb.org/3/'
+
+  showSpinner()
   const res = await fetch(
     `${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`
   )
   const data = await res.json()
+  console.log(data)
+  hideSpinner()
   return data
 }
 
@@ -48,12 +76,20 @@ const setActiveLink = () => {
   })
 }
 
+const showSpinner = () => {
+  document.querySelector('.spinner').classList.add('show')
+}
+
+const hideSpinner = () => {
+  document.querySelector('.spinner').classList.remove('show')
+}
+
 function init() {
   // Create a simple router
   switch (global.currentPage) {
     case '/':
     case '/index.html':
-      console.log('home page', window.location.pathname)
+      displayPopularMovies()
       break
     case '/movie-details.html':
       console.log('details: movie')
@@ -62,7 +98,7 @@ function init() {
       console.log('search')
       break
     case '/shows.html':
-      console.log('shows')
+      displayPopularShows()
       break
     case '/tv-details.html':
       console.log('details: tv')
@@ -71,7 +107,6 @@ function init() {
       break
   }
   setActiveLink()
-  displayPopularMovies()
 }
 
 document.addEventListener('DOMContentLoaded', init)
