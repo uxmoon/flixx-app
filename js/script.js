ddf858a9610656ccc4003d8b7b5ab476f2940974
uxmoon <uxmoon@gmail.com>
2023-04-18 11:56:35 -0300

    Set and display slider for latest movies

--- js/script.js
+++ js/script.js
@@ -229,11 +229,51 @@ const displayBackgroundImage = (type, imagePath) => {
   }
 }
 
+const displaySlider = async () => {
+  const { results } = await fetchAPIData('movie/now_playing')
+  results.forEach((movie) => {
+    const div = document.createElement('div')
+    div.classList.add('swiper-slide')
+    div.innerHTML = `
+      <a href="movie-details.html?id=${movie.id}">
+        <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}" />
+      </a>
+      <h4><i class="fas fa-star"></i> ${movie.vote_average} / 10</h4>`
+    document.querySelector('.swiper-wrapper').appendChild(div)
+    initSlider()
+  })
+}
+
+const initSlider = () => {
+  const swiper = new Swiper('.swiper', {
+    slidesPerView: 1,
+    spaceBetween: 32,
+    freeMode: true,
+    loop: true,
+    autoplay: {
+      delay: 4000,
+      disableOnInteraction: false,
+    },
+    breakpoints: {
+      576: {
+        slidesPerView: 2,
+      },
+      768: {
+        slidesPerView: 3,
+      },
+      1200: {
+        slidesPerView: 4,
+      },
+    },
+  })
+}
+
 function init() {
   // Create a simple router
   switch (global.currentPage) {
     case '/':
     case '/index.html':
+      displaySlider()
       displayPopularMovies()
       break
     case '/movie-details.html':
